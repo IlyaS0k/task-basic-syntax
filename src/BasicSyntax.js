9bbd77dbe6fc48de4764f70de1f8dfd3ebe40ddb
IlyaS0k <ilyasokolov63@mail.ru>
2022-03-26 16:23:17 +0300

    basic-task-syntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,22 @@
+const mp = new Map();
+mp.set('I', 1);
+mp.set('V', 5);
+mp.set('X', 10);
+mp.set('L', 50);
+mp.set('C', 100);
+mp.set('D', 500);
+mp.set('M', 1000);
 export function romanToInteger(str) {
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    for (let i = 0; i < str.length; ) {
+        if (mp.get(str[i]) < mp.get(str[i + 1])) {
+            result += mp.get(str[i + 1]) - mp.get(str[i]);
+            i += 2;
+        } else {
+            result += mp.get(str[i]);
+            ++i;
+        }
+    }
+
     return result;
 }
